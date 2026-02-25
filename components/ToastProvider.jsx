43e6ce40e0cf71ede299a82643f6cfd63c0cb69f
Hashir-3105 <hasnatsarwar139@gmail.com>
2026-02-25 13:21:45 +0500

    Add ToastProvider component for toast notifications

---
+++ components/ToastProvider.jsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+import { Toaster } from "sonner";
+
+export default function ToastProvider() {
+  return <Toaster position="bottom-right" richColors />;
+}
