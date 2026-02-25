684aebb80c4041a7fe7315607164b8040eb75d92
Hashir-3105 <hasnatsarwar139@gmail.com>
2026-02-25 13:19:05 +0500

    Add FormInput and FormRadio components during refactor

---
+++ components/form/form-input.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import { Field, FieldError, FieldLabel } from "../ui/field";
+import { Input } from "../ui/input";
+
+export default function FormInput({ name, label, type = "text", ...props }) {
+  const { control } = useFormContext();
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState }) => (
+        <Field data-invalid={fieldState.invalid}>
+          <FieldLabel> {label} </FieldLabel>
+          <Input
+            type={type}
+            {...field}
+            {...props}
+            aria-invalid={fieldState.invalid}
+          />
+          {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+        </Field>
+      )}
+    />
+  );
+}
---
+++ components/form/form-radio.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import {
+  Field,
+  FieldContent,
+  FieldError,
+  FieldLabel,
+  FieldTitle,
+} from "../ui/field";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+
+export default function FormRadio({ name, label, options }) {
+  const { control } = useFormContext();
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState }) => (
+        <Field data-invalid={fieldState.invalid}>
+          <FieldLabel>{label}</FieldLabel>
+          <RadioGroup
+            value={field.value}
+            onValueChange={field.onChange}
+            className="grid grid-cols-1 sm:grid-cols-2 gap-4"
+          >
+            {options.map((option) => {
+              return (
+                <FieldLabel key={option.value} htmlFor={option.value}>
+                  <Field orientation="horizontal">
+                    <RadioGroupItem value={option.value} id={option.value} />
+                    <FieldContent>
+                      <FieldTitle>{option.label}</FieldTitle>
+                    </FieldContent>
+                  </Field>
+                </FieldLabel>
+              );
+            })}
+          </RadioGroup>
+
+          {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+        </Field>
+      )}
+    />
+  );
+}
