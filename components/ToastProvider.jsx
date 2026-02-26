dcb26df3894f8f6721c676b1f929920a1ce1c6f6
Hashir-3105 <hasnatsarwar139@gmail.com>
2026-02-26 12:11:26 +0500

    improve theming, update ToastProvider, fix utils import, enhance queryType enum

--- components/ToastProvider.jsx
+++ components/ToastProvider.jsx
@@ -3,5 +3,5 @@ import React from "react";
 import { Toaster } from "sonner";
 
 export default function ToastProvider() {
-  return <Toaster position="bottom-right" richColors />;
+  return <Toaster position="bottom-right" />;
 }
